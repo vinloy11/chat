8cef98dc8f64a32ecb2dab5533c29d85141f3ede
Vinloy11 <a.shtork@mail.ru>
2019-12-04 23:34:50 +0700

    Added video broadcast

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ const io = require('socket.io')(http);
 app.get('/chat', function(req, res){
     res.sendFile(__dirname + '/socket.html');
 });
+let connectedClients = [];
 io.on('connection', function(socket){
     console.log('a user connected');
+    connectedClients.push(socket);
     socket.on('chat message', function(msg){
+        console.log(msg);
         io.emit( 'chat message' , msg);
+    });
+    socket.on('stream', function (stream) {
+        io.emit('stream', stream)
     });
     socket.on('disconnect', function(){
         console.log('user disconnected from your channel');
